3c1d0bb6490e45e9abeba5dc5ae63a5d850d9908
Emerson Samsonowski <emersonrs@gmail.com>
2020-07-15 00:25:09 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultaCEP/Controllers/ConsultaCEPController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using ConsultaCEP.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+//Aluno: Emerson Richter Samsonowski
+namespace ConsultaCEP.Controllers
+{
+    public class ConsultaCEPController : Controller
+    {
+        private readonly Context _context;
+        public ConsultaCEPController(Context context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            if (TempData["ConsultaCEP"] != null)
+            {
+                string jsonCEP = TempData["ConsultaCEP"].ToString();
+                Cep cep = JsonConvert.DeserializeObject<Cep>(jsonCEP);
+
+                _context.Ceps.Add(cep);
+                _context.SaveChanges();
+
+                return View(_context.Ceps.ToList());
+            }
+            return View(_context.Ceps.ToList());
+        }
+
+        [HttpPost]
+        public IActionResult ConsultarCEP(string txtCep)
+        {
+            string url =
+                $@"https://viacep.com.br/ws/{txtCep}/json/";
+            WebClient cliente = new WebClient();
+            TempData["ConsultaCEP"] = cliente.DownloadString(url);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ ConsultaCEP/Migrations/20200713004439_CreateDatabase.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsultaCEP.Migrations
+{
+    public partial class CreateDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cep",
+                columns: table => new
+                {
+                    cep = table.Column<string>(nullable: false),
+                    logradouro = table.Column<string>(nullable: true),
+                    complemento = table.Column<string>(nullable: true),
+                    bairro = table.Column<string>(nullable: true),
+                    localidade = table.Column<string>(nullable: true),
+                    uf = table.Column<string>(nullable: true),
+                    unidade = table.Column<string>(nullable: true),
+                    ibge = table.Column<string>(nullable: true),
+                    gia = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cep", x => x.cep);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cep");
+        }
+    }
+}
